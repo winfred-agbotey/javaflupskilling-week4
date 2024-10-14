f45b7c642ab77c2f4314fa06ea761827e5f185bf
winfred-agbotey <winfred.mawuli-agbotey@amalitech.com>
2024-10-14 09:24:48 +0000

    completed partone

---
+++ partone/src/test/java/com/mawulidev/week4/PartoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mawulidev.week4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
